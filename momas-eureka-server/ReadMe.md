be3745b57f1f3e16165f491ec85e3dbfcff92057
Sod-Momas <sothereer@gmail.com>
2021-01-24 19:55:13 +0800

    add eureka-server

---
+++ momas-eureka-server/ReadMe.md
@@ -0,0 +1,5 @@
+# momas-eureka-server
+
+Eureka 服务端
+
+启动后访问 `http://${eureka.instance.hostname}:${server.port}` (例如 http://localhost:2002 ) 可以访问Eureka前端
---
+++ momas-eureka-server/src/test/java/cc/momas/eureka/MomasEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.momas.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomasEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
