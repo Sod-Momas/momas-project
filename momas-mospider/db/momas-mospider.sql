e14843d5f6c949ed6f9c85b1b51f8f0b3e2ac027
Sod-Momas <sothereer@gmail.com>
2021-12-05 17:58:05 +0800

    add database table

--- momas-mospider/db/momas-mospider.sql
+++ momas-mospider/db/momas-mospider.sql
@@ -9,5 +9,19 @@ CREATE TABLE `momas-mospider`.`spider_data` (
   `insert_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
 COMMENT = '蜘蛛数据表';
+
+CREATE TABLE `momas-mospider`.`video_detail` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `page_url` VARCHAR(128) NULL COMMENT '页面地址',
+  `page_title` VARCHAR(128) NULL COMMENT '页面标题',
+  `video_size` VARCHAR(45) NULL COMMENT '视频体积',
+  `video_duration` VARCHAR(45) NULL COMMENT '视频时长',
+  `video_revlution` VARCHAR(45) NULL COMMENT '视频分辨率',
+  `video_format` VARCHAR(45) NULL COMMENT '视频格式',
+  `video_publish_date` VARCHAR(45) NULL COMMENT '视频发布日期',
+  `video_magnet` VARCHAR(2048) NULL COMMENT '视频磁力链接',
+  `vadeo_torrent_link` VARCHAR(2048) NULL COMMENT '视频torrent文件下载地址',
+  PRIMARY KEY (`id`))
+COMMENT = '视频详情列表';
